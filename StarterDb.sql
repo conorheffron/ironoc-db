848834f2adac06de8817691b39be890434a1d095
Conor Heffron <conor.heffron@gmail.com>
2014-11-25 21:28:01 +0000

    Update DB script to use upper case table name

--- StarterDb.sql
+++ StarterDb.sql
@@ -1,26 +1,26 @@
-DROP TABLE `test`.`person`;
+DROP TABLE `test`.`PERSON`;
 
-CREATE TABLE `test`.`person` (
+CREATE TABLE `test`.`PERSON` (
   `id` INT NULL AUTO_INCREMENT,
   `title` VARCHAR(45) NOT NULL,
   UNIQUE INDEX `id_UNIQUE` (`id` ASC))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_unicode_ci
-COMMENT = 'test person table';
+COMMENT = 'test PERSON table';
 
 
-INSERT INTO `test`.`person` (`id`, `title`) VALUES ("1", "Mr Conor");
+INSERT INTO `test`.`PERSON` (`id`, `title`) VALUES ("1", "Mr Conor");
 
-INSERT INTO `test`.`person` (`id`, `title`) VALUES ("2", "Mr John");
+INSERT INTO `test`.`PERSON` (`id`, `title`) VALUES ("2", "Mr John");
 
-INSERT INTO `test`.`person` (`id`, `title`) VALUES ("3", "Joe Bloggs");
+INSERT INTO `test`.`PERSON` (`id`, `title`) VALUES ("3", "Joe Bloggs");
 
-ALTER TABLE `test`.`person` 
+ALTER TABLE `test`.`PERSON` 
 ADD COLUMN `first_name` VARCHAR(45) NULL AFTER `title`,
 ADD COLUMN `surname` VARCHAR(45) NULL AFTER `first_name`,
 ADD COLUMN `age` INT NULL AFTER `surname`;
 
-UPDATE `test`.`person` SET `title`="Mr", `first_name`="Conor", `surname`="Heffron", `age`="30" WHERE `id`="1";
-UPDATE `test`.`person` SET `title`="Mr", `first_name`="Joe", `surname`="Bloggs", `age`="40" WHERE `id`="2";
-UPDATE `test`.`person` SET `title`="Dr", `first_name`="John", `surname`="Jones", `age`="20" WHERE `id`="3";
+UPDATE `test`.`PERSON` SET `title`="Mr", `first_name`="Conor", `surname`="Heffron", `age`="30" WHERE `id`="1";
+UPDATE `test`.`PERSON` SET `title`="Mr", `first_name`="Joe", `surname`="Bloggs", `age`="40" WHERE `id`="2";
+UPDATE `test`.`PERSON` SET `title`="Dr", `first_name`="John", `surname`="Jones", `age`="20" WHERE `id`="3";
