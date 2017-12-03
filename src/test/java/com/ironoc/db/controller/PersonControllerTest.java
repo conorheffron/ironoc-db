25fc2d3e527ccc8217a4952525419c14a3b5c011
Conor Heffron <conor.heffron@gmail.com>
2017-12-03 13:57:04 -0500

    Adding test template for controller class

---
+++ src/test/java/com/ironoc/db/controller/PersonControllerTest.java
@@ -0,0 +1,29 @@
+package com.ironoc.db.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class PersonControllerTest {
+
+    @Before
+    public void setUp() {
+
+    }
+
+    @Test
+    public void testCatchAll() {
+    }
+
+    @Test
+    public void testHome() {
+    }
+
+    @Test
+    public void testAddperson() {
+    }
+
+    @Test
+    public void testDeletePersonBySurname() {
+    }
+
+}
