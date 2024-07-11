79b68f7865d42ac641320ab22241662b3b95e029
Conor H <conor.heffron@gmail.com>
2024-07-11 17:52:59 +0100

    Refactor integration tests

--- src/integration-test/java/com.ironoc.db.controller/PersonControllerIntegrationTest.java
+++ src/integration-test/java/com.ironoc.db.controller/PersonControllerIntegrationTest.java
@@ -1,22 +1,27 @@
 package com.ironoc.db.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ironoc.db.config.AppConfig;
 import com.ironoc.db.dao.PersonDao;
 import com.ironoc.db.model.Person;
 import com.ironoc.db.service.PersonService;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import java.util.Collections;
 import java.util.List;
@@ -31,15 +36,27 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@WebMvcTest(PersonController.class)
-@AutoConfigureMockMvc
+//@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.MOCK)
+@WebMvcTest(controllers = PersonController.class)
+@TestPropertySource(properties = {"spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration"})
+
+@ContextConfiguration(classes = AppConfig.class)
+//@ExtendWith(SpringExtension.class)
+//@SpringBootTest(classes = {PersonController.class}, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//@AutoConfigureMockMvc
+//@RunWith(SpringRunner.class)
 public class PersonControllerIntegrationTest {
 
     @Autowired
+    private WebApplicationContext webAppContext;
+
+//    @Autowired
+//    private ApplicationContext context;
+
     private MockMvc mockMvc;
 
-    @Autowired
-    private ObjectMapper mapper;
+//    @Autowired
+    private ObjectMapper mapper = new ObjectMapper();
 
     @Mock
     private PersonService personServiceMock;
@@ -49,6 +66,12 @@ public class PersonControllerIntegrationTest {
 
     @InjectMocks
     private PersonController personController;// controller under test
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        mockMvc = MockMvcBuilders.webAppContextSetup(webAppContext).build();
+    }
 
     @Test
     public void test_addPerson_success() throws Exception {
@@ -120,4 +143,5 @@ public class PersonControllerIntegrationTest {
         assertThat(response.getForwardedUrl(), is(nullValue()));
         assertThat(response.getContentAsString(), is(emptyString()));
     }
+
 }
