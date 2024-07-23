6f1833beac17e888123112cfcc1a27edaccc3b0b
Conor Heffron <conor.heffron@gmail.com>
2024-07-23 19:53:35 +0100

    Update StarterDb.sql (#169)
    * Update StarterDb.sql
    
    * Update StarterDb.sql
    
    * Update StarterDb.sql
    
    * Update StarterDb.sql

--- db/StarterDb.sql
+++ db/StarterDb.sql
@@ -1,5 +1,4 @@
 DROP TABLE IF EXISTS `test`.`person`;
-
 DROP SCHEMA IF EXISTS `test`;
 
 CREATE SCHEMA `test`;
@@ -7,29 +6,16 @@ CREATE SCHEMA `test`;
 CREATE TABLE `test`.`person` (
   `id` INT NULL AUTO_INCREMENT,
   `title` VARCHAR(45) NOT NULL,
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8
-COLLATE = utf8_unicode_ci
-COMMENT = 'test person table';
-
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC));
 
 INSERT INTO `test`.`person` (`id`, `title`) VALUES ("1000", "Mr Conor");
-
 INSERT INTO `test`.`person` (`id`, `title`) VALUES ("1001", "Mr John");
-
 INSERT INTO `test`.`person` (`id`, `title`) VALUES ("1002", "Joe Bloggs");
 
-ALTER TABLE `test`.`person` 
-ADD COLUMN `first_name` VARCHAR(45) NULL AFTER `title`,
-ADD COLUMN `surname` VARCHAR(45) NULL AFTER `first_name`,
-ADD COLUMN `age` INT NULL AFTER `surname`;
+ALTER TABLE `test`.`person` ADD COLUMN `first_name` VARCHAR(45) NULL AFTER `title`, ADD COLUMN `surname` VARCHAR(45) NULL AFTER `first_name`, ADD COLUMN `age` INT NULL AFTER `surname`;
 
-UPDATE `test`.`person` SET `title`="Mr", `first_name`="Conor", `surname`="Heffron", `age`="30" WHERE `id`="1";
-UPDATE `test`.`person` SET `title`="Mr", `first_name`="Joe", `surname`="Bloggs", `age`="40" WHERE `id`="2";
-UPDATE `test`.`person` SET `title`="Dr", `first_name`="John", `surname`="Jones", `age`="20" WHERE `id`="3";
+UPDATE `test`.`person` SET `title`="Mr", `first_name`="Conor", `surname`="Heffron", `age`="30" WHERE `id`="1000";
+UPDATE `test`.`person` SET `title`="Mr", `first_name`="Joe", `surname`="Bloggs", `age`="40" WHERE `id`="1001";
+UPDATE `test`.`person` SET `title`="Dr", `first_name`="John", `surname`="Jones", `age`="20" WHERE `id`="1002";
 
--- ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'mypassword';
 ALTER USER 'root' IDENTIFIED WITH mysql_native_password BY 'mypassword';
-
- show variables like 'tx_isolation';
