26bde70208a95d373b1c5688792e8c7e4a62e091
Conor H <conor.heffron@gmail.com>
2024-08-27 15:52:44 +0100

    Update Person.java

--- src/main/java/com/ironoc/db/model/Person.java
+++ src/main/java/com/ironoc/db/model/Person.java
@@ -5,7 +5,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.persistence.Table;
 
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.NotEmpty;
