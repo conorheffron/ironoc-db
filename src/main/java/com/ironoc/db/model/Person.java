72e72859a39d0c33ddfc0cc644ad14263d915875
Conor Heffron <conor.heffron@gmail.com>
2024-09-10 02:57:12 +0100

    Update Model Validation & SQL load script (#194)
    * Update Model Validation & SQL load script
    
    Note: mysql image update external port to 3307
    
    * Update build.gradle
    
    Release version bump
    
    * Update Person.java
    
    * Remove get/sets for Lombok & improve logging info

--- src/main/java/com/ironoc/db/model/Person.java
+++ src/main/java/com/ironoc/db/model/Person.java
@@ -10,9 +10,12 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
 
 @Entity
 @Table(name="person")
+@Data
 public class Person {
 	
 	@Id
@@ -21,62 +24,25 @@ public class Person {
 	private Integer id;
 	
 	@Column(name="title")
-	@NotEmpty(message = "Title is not defined")
+	@NotEmpty(message = "Title is not defined.")
+	@Size(min = 2, max = 5, message = "Title should be between 2-5 characters.")
 	private String title;
 	
 	@Column(name="first_name")
-	@NotEmpty(message = "First Name is not defined")
+	@NotEmpty(message = "First Name is not defined.")
+	@Size(min = 3, max = 30, message = "First Name should be between 3-30 characters.")
 	private String firstName;
 	
 	@Column(name="surname")
-	@NotEmpty(message = "Surname is not defined")
+	@NotEmpty(message = "Surname is not defined.")
+	@Size(min = 3, max = 30, message = "Surname should be between 3-30 characters.")
 	private String surname;
 	
 	@Column(name="age")
-	@Min(value=1, message="Age is less than 1")
-	@Max(value=90, message="Age is greater than 90")
-	@NotNull(message = "Age is not defined")
+	@Min(value=1, message="Age is less than 1.")
+	@Max(value=90, message="Age is greater than 90.")
+	@NotNull(message = "Age is not defined.")
 	private Integer age;
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getSurname() {
-		return surname;
-	}
-
-	public void setSurname(String surname) {
-		this.surname = surname;
-	}
-
-	public Integer getAge() {
-		return age;
-	}
-
-	public void setAge(Integer age) {
-		this.age = age;
-	}
-
-	public Integer getId() {
-		return id;
-	}
-	
-	public void setId(Integer id) {
-		this.id = id;
-	}
-	
-	public String getTitle() {
-		return title;
-	}
-	
-	public void setTitle(String title) {
-		this.title = title;
-	}
 
 	public static class PersonBuilder {
 
