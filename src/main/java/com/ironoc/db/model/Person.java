7d8e902e8bb6da1f4ecafe6ef747b2c3b0774fe6
Conor H <conor.heffron@gmail.com>
2024-09-10 02:43:00 +0100

    Update Person.java

--- src/main/java/com/ironoc/db/model/Person.java
+++ src/main/java/com/ironoc/db/model/Person.java
@@ -6,7 +6,11 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
-import jakarta.validation.constraints.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name="person")
