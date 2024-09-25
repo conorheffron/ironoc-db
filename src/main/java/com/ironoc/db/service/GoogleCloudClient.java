4549425ce3042f966640e43a2d4817a7162feede
Conor H <conor.heffron@gmail.com>
2024-09-25 21:43:57 +0100

    #245 Refactor & extract GCP secrets service from H2 data src

---
+++ src/main/java/com/ironoc/db/service/GoogleCloudClient.java
@@ -0,0 +1,5 @@
+package com.ironoc.db.service;
+
+public interface GoogleCloudClient {
+    String getSecret(String secretVersion);
+}
---
+++ src/main/java/com/ironoc/db/service/GoogleCloudClientImpl.java
@@ -0,0 +1,27 @@
+package com.ironoc.db.service;
+
+import com.google.cloud.secretmanager.v1.AccessSecretVersionResponse;
+import com.google.cloud.secretmanager.v1.SecretManagerServiceClient;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Marker;
+import org.slf4j.MarkerFactory;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class GoogleCloudClientImpl implements GoogleCloudClient {
+
+    @Override
+    public String getSecret(String secretVersion) {
+        String secretValue = "";
+        try {
+            SecretManagerServiceClient client = SecretManagerServiceClient.create();
+            AccessSecretVersionResponse response = client.accessSecretVersion(secretVersion);
+            secretValue = response.getPayload().getData().toStringUtf8();
+            log.info("Extracting GCP secret key={}", secretVersion);
+        } catch (Exception e) {
+            log.error("Unexpected error occurred extracting GCP secret.", e);
+        }
+        return secretValue;
+    }
+}
