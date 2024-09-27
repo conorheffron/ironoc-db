57ff2d29806248c66a235081442b8925adedba03
Conor Heffron <conor.heffron@gmail.com>
2024-09-27 17:47:48 +0100

    #201 #202 Update data-h2.sql, data-mysql.sql & persons.sql (#249)

--- src/main/resources/db/data-h2.sql
+++ src/main/resources/db/data-h2.sql
@@ -1,5 +1,4 @@
---h2
+-- h2 initial data load INSERTS
 INSERT INTO IRONOC_DB.PERSON (id, title, first_name, surname, age) VALUES (1001, 'Mr', 'Conor', 'Heffron', 45);
 INSERT INTO IRONOC_DB.PERSON (id, title, first_name, surname, age) VALUES (1002, 'Mrs.', 'Nora', 'Jones', 29);
 INSERT INTO IRONOC_DB.PERSON (id, title, first_name, surname, age) VALUES (1003, 'Dr', 'Joe', 'Bloggs', 43);
---SELECT * FROM PERSON where title LIKE 'M%'; from H2 console
