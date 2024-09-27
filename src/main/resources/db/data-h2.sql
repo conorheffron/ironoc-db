5704ef718734c0088e9e4c97a80805d97e768917
Conor H <conor.heffron@gmail.com>
2024-09-27 02:21:52 +0100

    #214 #212 #211 Update application-h2.properties & data-h2.sql

--- src/main/resources/db/data-h2.sql
+++ src/main/resources/db/data-h2.sql
@@ -1,5 +1,5 @@
 --h2
-INSERT INTO PERSON (id, title, first_name, surname, age) VALUES (1001, 'Mr', 'Conor', 'Heffron', 45);
-INSERT INTO PERSON (id, title, first_name, surname, age) VALUES (1002, 'Mrs.', 'Nora', 'Jones', 29);
-INSERT INTO PERSON (id, title, first_name, surname, age) VALUES (1003, 'Dr', 'Joe', 'Bloggs', 43);
+INSERT INTO IRONOC_DB.PERSON (id, title, first_name, surname, age) VALUES (1001, 'Mr', 'Conor', 'Heffron', 45);
+INSERT INTO IRONOC_DB.PERSON (id, title, first_name, surname, age) VALUES (1002, 'Mrs.', 'Nora', 'Jones', 29);
+INSERT INTO IRONOC_DB.PERSON (id, title, first_name, surname, age) VALUES (1003, 'Dr', 'Joe', 'Bloggs', 43);
 --SELECT * FROM PERSON where title LIKE 'M%'; from H2 console
