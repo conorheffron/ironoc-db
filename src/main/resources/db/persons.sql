d952a7173530c063301eccb442d091cf8ef8a5be
Conor Heffron <conor.heffron@gmail.com>
2024-09-27 18:38:59 +0100

    Bsql (#260)
    * #201 #202 Update data-h2.sql, data-mysql.sql & persons.sql
    
    * #259 Update persons.sql

--- src/main/resources/db/persons.sql
+++ src/main/resources/db/persons.sql
@@ -4,20 +4,17 @@ DROP SCHEMA IF EXISTS IRONOC_DB;
 -- CREATE TABLES
 CREATE SCHEMA IRONOC_DB;
 CREATE TABLE IRONOC_DB.PERSON (
-  `age` INT NOT NULL,
-  `id` INT NULL AUTO_INCREMENT,
-  `first_name` VARCHAR(30) NOT NULL,
-  `surname` VARCHAR(30) NOT NULL,
-  `title` VARCHAR(5) NOT NULL,
-  UNIQUE (`id` ASC)
+  age INT NOT NULL,
+  id INT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  surname VARCHAR(30) NOT NULL,
+  title VARCHAR(5) NOT NULL,
+  UNIQUE (id ASC)
 );
--- Enter passw as ENV VAR for docker image or alter here later via SQL client
+-- *** Enter passw as ENV VAR for docker image or alter here later via SQL client
 -- ALTER USER 'root' IDENTIFIED WITH mysql_native_password BY 'mypassword';
--- View Data
--- SELECT `person`.`age`,
---    `person`.`id`,
---    `person`.`title`,
---    `person`.`first_name`,
---    `person`.`surname`
--- FROM `ironoc_db`.`person`;
--- SELECT * FROM IRONOC_DB.PERSON where title LIKE 'M%'; from H2 console
+-- *** View Data
+-- SELECT p1.age, -- p1.id, p1.title, p1.first_name, p1.surname AS p_surname
+--  FROM IRONOC_DB.PERSON AS p1;
+-- *** from H2 console
+-- SELECT * FROM IRONOC_DB.PERSON where title LIKE 'M%';
