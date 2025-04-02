c92cfae01b9ac835eb251da0b1f54f0cce043c00
Conor Heffron <conor.heffron@gmail.com>
2025-04-02 17:04:19 +0100

    Release v6.1.3

--- src/main/resources/db/data-h2.sql
+++ src/main/resources/db/data-h2.sql
@@ -1,9 +1,9 @@
 -- h2 initial data load INSERTS
-INSERT INTO IRONOC_DB.PERSON (id, title, first_name, surname, age) VALUES (1001, 'Mr', 'Conor', 'Heffron', 45);
-INSERT INTO IRONOC_DB.PERSON (id, title, first_name, surname, age) VALUES (1002, 'Mrs.', 'Nora', 'Jones', 29);
-INSERT INTO IRONOC_DB.PERSON (id, title, first_name, surname, age) VALUES (1003, 'Dr', 'Joe', 'Bloggs', 43);
+INSERT INTO IRONOC_DB.PERSON (id, title, first_name, surname, age) VALUES (1000, 'Ms', 'Sade', 'Song', 47);
+INSERT INTO IRONOC_DB.PERSON (id, title, first_name, surname, age) VALUES (2000, 'Mrs', 'Nora', 'Jones', 29);
+INSERT INTO IRONOC_DB.PERSON (id, title, first_name, surname, age) VALUES (3000, 'Ms', 'Halle', 'Movie', 47);
 
-INSERT INTO IRONOC_DB.EMPLOYER (employer_id, employee_id, title, employer_name, start_year) VALUES (2001, 1001, 'Software / Data Engineer', 'Moragn Stanley', 2017);
-INSERT INTO IRONOC_DB.EMPLOYER (employer_id, employee_id, title, employer_name, start_year) VALUES (2002, 1001, 'Software Developer', 'BSkyB', 2014);
-INSERT INTO IRONOC_DB.EMPLOYER (employer_id, employee_id, title, employer_name, start_year) VALUES (2003, 1002, 'Software Engineer', 'Spotify', 2024);
-INSERT INTO IRONOC_DB.EMPLOYER (employer_id, employee_id, title, employer_name, start_year) VALUES (2004, 1003, 'Data Engineer', 'Google', 2025);
+INSERT INTO IRONOC_DB.EMPLOYER (employer_id, employee_id, title, employer_name, start_year) VALUES (2001, 1000, 'Software / Data Engineer', 'Morgan Stanley', 2017);
+INSERT INTO IRONOC_DB.EMPLOYER (employer_id, employee_id, title, employer_name, start_year) VALUES (2002, 1000, 'Software Developer', 'BSkyB', 2014);
+INSERT INTO IRONOC_DB.EMPLOYER (employer_id, employee_id, title, employer_name, start_year) VALUES (2003, 2000, 'Software Engineer', 'Spotify', 2024);
+INSERT INTO IRONOC_DB.EMPLOYER (employer_id, employee_id, title, employer_name, start_year) VALUES (2004, 3000, 'Data Engineer', 'Google', 2025);
