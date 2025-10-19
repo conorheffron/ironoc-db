87137fc88b05ba8bb22994ce212c2deef9dad663
Conor Heffron <conor.heffron@gmail.com>
2025-10-19 08:55:51 +0100

    Update CustomErrorControllerIntegrationTest.java

--- src/integration-test/java/com.ironoc.db.controller/CustomErrorControllerIntegrationTest.java
+++ src/integration-test/java/com.ironoc.db.controller/CustomErrorControllerIntegrationTest.java
@@ -19,8 +19,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
+import module static org.hamcrest;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
