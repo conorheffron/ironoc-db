c6aa9462e0fd966406ae4b5bcd54a673f611002c
Conor Heffron <conor.heffron@gmail.com>
2025-10-19 08:47:49 +0100

    Update App.java

--- src/main/java/com/ironoc/db/App.java
+++ src/main/java/com/ironoc/db/App.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class App {
 
-	void main(String[] args) {
+	public void main(String[] args) {
 		SpringApplication.run(App.class, args);
 	}
 }
