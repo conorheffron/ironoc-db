7280f45f0648116d34091a28b90b188147b5d529
Conor Heffron <conor.heffron@gmail.com>
2025-10-19 08:59:42 +0100

    Update VersionController.java

--- src/main/java/com/ironoc/db/controller/VersionController.java
+++ src/main/java/com/ironoc/db/controller/VersionController.java
@@ -1,9 +1,6 @@
 package com.ironoc.db.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.info.BuildProperties;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import module org.springframework;
 
 @ControllerAdvice
 public class VersionController {
