f32bb365dcf902a98b0e1d709a5b54fd4298bf45
Conor Heffron <conor.heffron@gmail.com>
2025-10-19 08:29:27 +0100

    Update PersonDao.java

--- src/main/java/com/ironoc/db/dao/PersonDao.java
+++ src/main/java/com/ironoc/db/dao/PersonDao.java
@@ -1,7 +1,6 @@
 package com.ironoc.db.dao;
 
-import java.util.List;
-import java.util.Optional;
+import module java.base;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
