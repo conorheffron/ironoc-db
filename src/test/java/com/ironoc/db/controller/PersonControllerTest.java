2ad2a50807424c4032930f95a7b4df7f300e8908
Conor Heffron <conor.heffron@gmail.com>
2026-05-11 14:04:57 +0100

    Refactor PersonControllerTest to use PersonDto

--- src/test/java/com/ironoc/db/controller/PersonControllerTest.java
+++ src/test/java/com/ironoc/db/controller/PersonControllerTest.java
@@ -2,6 +2,8 @@ package com.ironoc.db.controller;
 
 import module java.base;
 
+import com.ironoc.db.dto.PersonDto;
+import com.ironoc.db.mapper.PersonMapper;
 import com.ironoc.db.model.Person;
 import com.ironoc.db.service.PersonService;
 import org.junit.Before;
@@ -15,21 +17,28 @@ import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.Mockito.when;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class PersonControllerTest {
 
     @InjectMocks
-    private PersonController personController;// controller under test
+    private PersonController personController;
 
     @Mock
     private PersonService personServiceMock;
+
+    @Mock
+    private PersonMapper personMapperMock;
 
     @Mock
     private ModelMap modelMapMock;
@@ -41,44 +50,39 @@ public class PersonControllerTest {
     private Person personMock;
 
     @Mock
+    private PersonDto personDtoMock;
+
+    @Mock
     private BindingResult bindingResultMock;
 
-    // test variables
     private static final Integer TEST_ID = 7;
     private List<Person> persons;
 
     @Before
     public void setUp() {
-        // initialise test variables
         persons = new ArrayList<>();
         persons.add(personMock);
     }
 
     @Test
     public void test_Home_success() {
-        // given
         when(personServiceMock.getAllPersons()).thenReturn(persons);
 
-        // when
         String result = personController.home(modelMapMock);
 
-        // then
         verify(personServiceMock).getAllPersons();
-
         assertThat(result, is("index"));
     }
 
     @Test
     public void test_AddPerson_success() {
-        // given
         when(bindingResultMock.hasErrors()).thenReturn(false);
-        when(personServiceMock.addPerson(ArgumentMatchers.any(Person.class))).thenReturn(true);
+        when(personMapperMock.toPerson(personDtoMock)).thenReturn(personMock);
 
-        // when
-        String result = personController.addPerson(modelMapMock, personMock, bindingResultMock);
+        String result = personController.addPerson(modelMapMock, personDtoMock, bindingResultMock);
 
-        // then
         verify(bindingResultMock).hasErrors();
+        verify(personMapperMock).toPerson(personDtoMock);
         verify(personServiceMock).addPerson(personMock);
         verify(personServiceMock, never()).getAllPersons();
 
@@ -87,29 +91,28 @@ public class PersonControllerTest {
 
     @Test
     public void test_updatePerson_fail() {
-        // given
         when(bindingResultMock.hasErrors()).thenReturn(true);
 
-        // when
-        String result = personController.updatePerson(modelMapMock, TEST_ID, personMock, bindingResultMock);
+        String result = personController.updatePerson(modelMapMock, TEST_ID, personDtoMock, bindingResultMock);
 
-        // then
         verify(bindingResultMock).hasErrors();
-        verify(personServiceMock, never()).addPerson(personMock);
+        verify(personDtoMock).setId(TEST_ID.longValue());
+        verify(modelMapMock).addAttribute("person", personDtoMock);
+        verify(personServiceMock, never()).addPerson(ArgumentMatchers.any(Person.class));
 
         assertThat(result, is("edit-person"));
     }
 
     @Test
     public void test_updatePerson_success() {
-        // given
         when(bindingResultMock.hasErrors()).thenReturn(false);
+        when(personMapperMock.toPerson(personDtoMock)).thenReturn(personMock);
 
-        // when
-        String result = personController.updatePerson(modelMapMock, TEST_ID, personMock, bindingResultMock);
+        String result = personController.updatePerson(modelMapMock, TEST_ID, personDtoMock, bindingResultMock);
 
-        // then
         verify(bindingResultMock).hasErrors();
+        verify(personDtoMock).setId(TEST_ID.longValue());
+        verify(personMapperMock).toPerson(personDtoMock);
         verify(personServiceMock).addPerson(personMock);
 
         assertThat(result, is("redirect:/"));
@@ -117,45 +120,38 @@ public class PersonControllerTest {
 
     @Test
     public void test_showEditView_success() {
-        // given
         when(personServiceMock.findPersonById(anyLong())).thenReturn(Optional.of(personMock));
+        when(personMapperMock.toPersonDto(personMock)).thenReturn(personDtoMock);
 
-        // when
         String result = personController.showEditView(TEST_ID, modelMock);
 
-        // then
         verify(personServiceMock).findPersonById(anyLong());
-        verify(modelMock).addAttribute("person", personMock);
+        verify(personMapperMock).toPersonDto(personMock);
+        verify(modelMock).addAttribute("person", personDtoMock);
 
         assertThat(result, is("edit-person"));
     }
 
     @Test
     public void test_showEditView_fail() {
-        // given
         when(personServiceMock.findPersonById(anyLong())).thenReturn(Optional.empty());
 
-        // when
         String result = personController.showEditView(TEST_ID, modelMock);
 
-        // then
         verify(personServiceMock).findPersonById(anyLong());
-        verify(modelMock, never()).addAttribute("person", personMock);
+        verify(modelMock, never()).addAttribute("person", personDtoMock);
 
         assertThat(result, is("edit-person"));
     }
 
     @Test
     public void test_AddPerson_fail() {
-        // given
         when(bindingResultMock.hasErrors()).thenReturn(true);
 
-        // when
-        String result = personController.addPerson(modelMapMock, personMock, bindingResultMock);
+        String result = personController.addPerson(modelMapMock, personDtoMock, bindingResultMock);
 
-        // then
         verify(bindingResultMock).hasErrors();
-        verify(personServiceMock, never()).addPerson(personMock);
+        verify(personServiceMock, never()).addPerson(ArgumentMatchers.any(Person.class));
         verify(personServiceMock).getAllPersons();
 
         assertThat(result, is("index"));
@@ -163,13 +159,10 @@ public class PersonControllerTest {
 
     @Test
     public void test_deletePersonById_success() {
-        // given
         when(personServiceMock.findPersonById(TEST_ID.longValue())).thenReturn(Optional.ofNullable(persons.getFirst()));
 
-        // when
         String result = personController.deletePersonById(modelMapMock, TEST_ID);
 
-        // then
         verify(personServiceMock).findPersonById(TEST_ID.longValue());
         verify(personServiceMock).deletePersonById(TEST_ID.longValue());
         verify(personServiceMock, never()).getAllPersons();
@@ -179,13 +172,10 @@ public class PersonControllerTest {
 
     @Test
     public void test_deletePersonById_fail() {
-        // given
         when(personServiceMock.findPersonById(TEST_ID.longValue())).thenReturn(Optional.empty());
 
-        // when
         String result = personController.deletePersonById(modelMapMock, TEST_ID);
 
-        // then
         verify(personServiceMock).findPersonById(TEST_ID.longValue());
         verify(personServiceMock, never()).deletePersonById(TEST_ID.longValue());
         verify(modelMapMock).addAttribute("deleteError", "There are no matching entries to delete");
